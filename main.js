4c0c84d5416fe88890950e9dbe35d926b9beb68e
NowWeAre-Free <kontodarm@wp.pl>
2023-07-02 11:01:40 +0200

    code works but needs optimalisation

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 // jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
 // jesli wartość dla x i y jest nieparzysta to kolor niebieski
 // jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
+
+document.body.addEventListener("click", (e) => {
+  const positionX = e.clientX;
+  const positionY = e.clientY;
+  console.log(positionX, positionY);
+  if (positionX % 2 === 0 && positionY % 2 === 0) {
+    document.body.style.backgroundColor = "red";
+  } else if (positionX % 2 !== 0 && positionY % 2 !== 0) {
+    document.body.style.backgroundColor = "blue";
+  } else {
+    document.body.style.backgroundColor = "green";
+  }
+});
